85ce9659c1fbd8a1b20a46e31cd0fd4ee0a45c75
Abigail Inwood <abigailinwood@Abigails-MacBook-Air.local>
2025-10-20 21:14:13 +0100

    Add Tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
